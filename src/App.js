3d026dca7e177136afa1a7b6a8e8403abf06ace6
Thomas <thomas.dicola@hotmail.fr>
2023-03-08 15:19:53 +0100

    08-03-23 refactor Wilder comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import blank_profile from "./assets/blank_profile.png";
 import "./App.css";
+import Wilder from "./components/Wilder";
 
 function App() {
   return (
@@ -12,38 +12,9 @@ function App() {
       <main className="container">
         <h2>Wilders</h2>
         <section className="card-row">
-          <article className="card">
-            <img src={blank_profile} alt="Jane Doe Profile" />
-            <h3>Jane Doe</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <h4>Wild Skills</h4>
-            <ul className="skills">
-              <li>
-                HTML
-                <span className="votes">3</span>
-              </li>
-              <li>
-                CSS
-                <span className="votes">3</span>
-              </li>
-              <li>
-                Typescript
-                <span className="votes">3</span>
-              </li>
-              <li>
-                React
-                <span className="votes">3</span>
-              </li>
-              <li>
-                Node <span className="votes">2</span>
-              </li>
-            </ul>
-          </article>
+          <Wilder />
+          <Wilder />
+          <Wilder />
         </section>
       </main>
       <footer>
---
+++ src/components/Wilder.js
@@ -0,0 +1,38 @@
+import blank_profile from "../assets/blank_profile.png";
+
+export default function Wilder() {
+  return (
+    <article className="card">
+      <img src={blank_profile} alt="Jane Doe Profile" />
+      <h3>Jane Doe</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      <h4>Wild Skills</h4>
+      <ul className="skills">
+        <li>
+          HTML
+          <span className="votes">3</span>
+        </li>
+        <li>
+          CSS
+          <span className="votes">3</span>
+        </li>
+        <li>
+          Typescript
+          <span className="votes">3</span>
+        </li>
+        <li>
+          React
+          <span className="votes">3</span>
+        </li>
+        <li>
+          Node <span className="votes">2</span>
+        </li>
+      </ul>
+    </article>
+  );
+}
